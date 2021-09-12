6f5acd8c922379e6a7373eb7a8717499f4dd1272
JuanTorres7 <juan.torres.f@utec.edu.pe>
2021-09-12 17:40:35 -0500

    sub_2

--- stack.h
+++ stack.h
@@ -1,6 +1,19 @@
 #include "circulararray.h"
+#include <memory>
 
 template <typename T>
-class StackArray : public CircularArray<T> { 
-    
+class StackArray : public CircularArray<T> {
+private:
+    unique_ptr<T[]> cA;
+    int head, size=0;
+public:
+    StackArray(T size) : CircularArray<T>(make_unique<T[]>(size)), size(size) {}
+    void push (T data) {
+        cA[head] = data;
+        head = (head+1) % size;
+    }
+    T pop() {
+        head = (head-1+size)%size;
+        return cA[head];
+    }
 };
